f3b979ff0798a2956f7b7d7eb4428e5283f6198f
Prasanna Sawant <prasanna.sawant@macmarts.com>
2024-07-09 15:59:55 +0530

    Finishing & Using the Cart Context & Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import { CartContextProvider } from "./store/CartContext";
+
+
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Meals />
-    </>
+    </CartContextProvider>
   );
 }
 
