191bbf749a74efbd952a1fcbb9dbdb27f61da3a7
Prasanna Sawant <prasanna.sawant@macmarts.com>
2024-07-09 14:43:48 +0530

    Creating a Configurable & Flexible Custom Button Component

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,14 +1,17 @@
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+
 export default function MealItem({meal}) {
     return <li className="meal-item">
         <article>
             <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
             <div>
                 <h3>{meal.name}</h3>
-                <p className="meal-item-price">{meal.price}</p>
+                <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
                 <p className="meal-item-description">{meal.description}</p>
             </div>
             <p className="meal-item-actions">
-                <button>Add to Card</button>
+                <Button>Add to Card</Button>
             </p>
         </article>
     </li>
