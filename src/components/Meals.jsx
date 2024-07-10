3f77ada00870c459013477333487f1292ce7b6d8
Prasanna Sawant <prasanna.sawant@macmarts.com>
2024-07-10 13:08:43 +0530

    Sending a POST Request with Order Data

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,27 +1,29 @@
-import { useState, useEffect } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
+const requestConfig = {};
+
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals",requestConfig,[]);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+  console.log(loadedMeals);
 
-      if (!response.ok) {
-        //...
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
+  if (isLoading) {
+    return <p>Fetching Meals...</p>;
+  }
 
-    fetchMeals();
-  }, []);
+  // if(!data){
+  //   return <p>No meals found.</p>
+  // }
 
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-   <MealItem key={meal.id} meal={meal}/>
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
