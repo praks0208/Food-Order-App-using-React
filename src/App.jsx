2c6f16624fd6f02198a69a656f84b748b58c3be2
Prasanna Sawant <prasanna.sawant@macmarts.com>
2024-07-10 11:43:45 +0530

    Adding a Custom Input Component & Managing Modal Visibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 import { CartContextProvider } from "./store/CartContext";
 import { UserProgressContextProvider } from "./store/UserProgressContext";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Header />
         <Meals />
         <Cart />
+        <Checkout/>
       </CartContextProvider>
     </UserProgressContextProvider>
   );
