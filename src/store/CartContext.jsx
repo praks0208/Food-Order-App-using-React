9b6fe78f7fbaf1f6916d3157dffeafa6694e0ea9
Prasanna Sawant <prasanna.sawant@macmarts.com>
2024-07-10 15:33:09 +0530

    Handling Checkout Submit and Cart Reset

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {}
 });
 
 function cartReducer(state, action) {
@@ -49,6 +50,10 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
+  if(action.type === 'CLEAR_CART'){
+    return {...state, items: []};
+  }
+
   return state;
 }
 
@@ -56,17 +61,22 @@ export function CartContextProvider({ children }) {
   const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
   function addItem(item) {
-    dispatchCartAction({ type: "ADD_ITEM", item });
+    dispatchCartAction({ type: 'ADD_ITEM', item });
   }
 
   function removeItem(id) {
-    dispatchCartAction({ type: "REMOVE_ITEM", id });
+    dispatchCartAction({ type: 'REMOVE_ITEM', id });
+  }
+
+  function clearCart(){
+    dispatchCartAction({type: 'CLEAR_CART'});
   }
 
   const cartContext = {
     items: cart.items,
     addItem,
     removeItem,
+    clearCart
   };
 
   return (
