d0d308821a6df0ebe2a0f284f7bb6799c63c5d4a
Prasanna Sawant <prasanna.sawant@macmarts.com>
2024-07-10 11:52:04 +0530

    Handling Form Submission & Validation

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -6,35 +6,49 @@ import Input from "./UI/Input";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
 
-export default function Checkout(){
-   const cartCtx = useContext(CartContext);
-   const userProgressCtx = useContext(UserProgressContext);
+export default function Checkout() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
-   const cartTotal = cartCtx.items.reduce(
-    (totalPrice, item) => totalPrice + item.quantity * item.price, 0
-   );
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
 
-   function handleClose(){
+  function handleClose() {
     userProgressCtx.hideCheckout();
-   }
+  }
 
-    return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-        <form>
-            <h2>Checkout</h2>
-            <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
+  function handleSubmit(event) {
+    event.preventDefault();
 
-            <Input label="Full Name" type="text" id="full-name"/>
-            <Input label="E-Mail Address" type="email" id="email" />
-            <Input label="Street" type="text" id="street"  />
-            <div className="control-row">
-            <Input label="Postal Code" type="text" id="postal-code"  />
-            <Input label="City" type="text" id="city"  />
-            </div>
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
 
-            <p className="modal-actions">
-                <Button type="button" textOnly onClick={handleClose}>Close</Button>
-                <Button>Submit Order</Button>
-            </p>
-        </form>
+    
+  }
+
+  return (
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form onSubmit={handleSubmit}>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
+
+        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="E-Mail Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+
+        <p className="modal-actions">
+          <Button type="button" textOnly onClick={handleClose}>
+            Close
+          </Button>
+          <Button>Submit Order</Button>
+        </p>
+      </form>
     </Modal>
+  );
 }
